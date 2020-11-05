07307dfeef428ebd10e81057b1f7ac6dfa720a30
Monesh Venkul <mvvenkul@gmail.com>
2020-11-05 17:24:20 +0530

    added spring boot application

---
+++ SpringBootBasicApplication/src/main/java/com/main/controller/LoginController.java
@@ -0,0 +1,29 @@
+package com.main.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class LoginController {
+
+ 
+
+    @GetMapping("/login")
+    public ModelAndView loginPage()
+    {
+        return new ModelAndView("login");
+    }
+    
+    @GetMapping("/register")
+    public ModelAndView registerPage()
+    {
+        return new ModelAndView("register");
+    }
+    
+    @GetMapping("/success")
+    public ModelAndView successPage()
+    {
+        return new ModelAndView("success");
+    }
+}
