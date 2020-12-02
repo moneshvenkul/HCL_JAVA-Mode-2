317df81026daf750d60de7a17b9e38c1318cb3a2
Monesh Venkul <mvvenkul@gmail.com>
2020-12-02 22:24:20 +0530

    Completed Registration and Login for Aeroplane Management System

--- Airport Management System/ServerSide/src/main/java/com/pack/ServerSide/SpringBootSecurityJwtApplication.java
+++ Airport Management System/ServerSide/src/main/java/com/pack/ServerSide/SpringBootSecurityJwtApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ServerSideApplication {
+public class SpringBootSecurityJwtApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ServerSideApplication.class, args);
+		SpringApplication.run(SpringBootSecurityJwtApplication.class, args);
 	}
 
 }
